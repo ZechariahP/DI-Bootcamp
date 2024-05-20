87b400b668a2007d42444ebb03671448dd36ddff
Zachary <zacharypremack@gmail.com>
2024-05-20 15:34:49 +0300

    Week 3 Day 4 Daily Challenge

---
+++ Week3/Day4/DailyChallenge/script.js
@@ -0,0 +1,18 @@
+const sentence = "This cat is not that bad, I like him."
+const wordNot = sentence.search("not");
+const wordBad = sentence.search("bad");
+
+
+
+if (wordNot === -1) {
+	console.log(sentence)
+} else if (wordNot < wordBad) {
+	console.log('wordBad:', wordBad)
+	console.log('wordNot:', wordNot)
+
+	const firstPart = sentence.slice(0, wordNot);
+	const secondPart = sentence.slice(wordBad + 3);
+	console.log(firstPart + "good" + secondPart);
+} else {
+	console.log(sentence);
+}
