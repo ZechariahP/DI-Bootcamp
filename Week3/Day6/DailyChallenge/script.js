b4eb1306cb4d6d1cb2448d769167b7ecd20ee42d
Zachary <zacharypremack@gmail.com>
2024-05-22 16:05:20 +0300

    Week 3 Day 6 Daily Challenge

---
+++ Week3/Day6/DailyChallenge/script.js
@@ -0,0 +1,15 @@
+const maxNumber = 6
+let stars = ""
+for (let i=0; i < maxNumber; i++) {
+	stars = stars + " * "
+	console.log(stars)
+}
+
+for (let i = 0;i < maxNumber;i++) {
+	const numberOfStars = i + 1
+	let lineOfStars = ""
+	for (let j = 0;j < numberOfStars;j++) {
+		lineOfStars = lineOfStars + " * "
+	}
+	console.log(lineOfStars)
+}
