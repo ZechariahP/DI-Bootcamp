e80896ef44165df5f761ab68415c0b7ad28fa083
Zachary <zacharypremack@gmail.com>
2024-06-17 14:10:39 +0300

    Resubmitting Daily Challenge

--- Week6/DailyChallenge/DailyChallenge.sql
+++ Week6/DailyChallenge/DailyChallenge.sql
@@ -13,46 +13,16 @@ CREATE DATABASE "Hollywood"
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
-CREATE TABLE actors(
-	actor_id SERIAL PRIMARY KEY,
-	first_name VARCHAR (50) NOT NULL,
-	last_name VARCHAR (100) NOT NULL,
-	age DATE NOT NULL,
-	number_oscars SMALLINT NOT NULL
-)
-
 SELECT * FROM actors;
 
-INSERT INTO actors (first_name, last_name, age, number_oscars)
-VALUES
-	('Matt','Damon','08/10/1970', 5),
-	('George','Clooney','06/05/1961', 2),
-	('Viola','Davis','08/11/1965', 1),
-	('Meryl','Streep','06/22/1949', 3),
-	('Chris','Pratt','06/21/1979', 0),
-	('Daniel','Day-Lewis','04/29/1957', 3),
-	('Katharine','Hepburn','05/12/1907', 4)
+SELECT COUNT(*) FROM actors;
 
-SELECT * FROM actors;
+INSERT INTO actors (first_name,last_name,birthdate,number_oscars)
+VALUES ('Gal','Gadot',NULL,NULL)
 
-UPDATE actors
-SET first_name='Maty' WHERE first_name='Matt' AND last_name='Damon' AND age='1970-08-10'
-RETURNING actor_id, first_name, last_name, age;
+-- Inserting another actor with blank fields will not work because the constraints say that all fields must be "NOT NULL".
+--ERROR:  Failing row contains (8, Gal, Gadot, null, null).null value in column "birthdate" of relation "actors" violates not-null constraint 
 
-SELECT * FROM actors;
-
-UPDATE actors
-SET number_oscars=4 WHERE first_name='George' AND last_name='Clooney' AND age='1961-06-05'
-RETURNING actor_id, first_name, last_name, age, number_oscars;
-
-SELECT * FROM actors;
-
-ALTER TABLE actors RENAME COLUMN age TO birthdate;
-
-SELECT * FROM actors;
-
-DELETE FROM actors
-WHERE actor_id=5;
-
-SELECT * FROM actors;
-
+--ERROR:  null value in column "birthdate" of relation "actors" violates not-null constraint
+--SQL state: 23502
+--Detail: Failing row contains (8, Gal, Gadot, null, null).
